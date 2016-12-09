0754e1ac084c7de1b9776fb8ce4037f2b72e57a9
07asier <asier2223@gmail.com>
2016-12-09 13:33:34 +0100

    Este es el 1er camio

--- app/src/main/java/com/example/asier/cebanc_burger/MainActivity.java
+++ app/src/main/java/com/example/asier/cebanc_burger/MainActivity.java
@@ -2,12 +2,16 @@ package com.example.asier.cebanc_burger;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText texto;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
 }
