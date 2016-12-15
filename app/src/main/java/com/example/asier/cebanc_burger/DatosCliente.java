33a7bbb597f56af7e9c8bda8f85d7af890673a2e
joniggi <patexample04@gmail.com>
2016-12-15 10:25:28 +0100

    Este es el cambio de los if!!!

--- app/src/main/java/com/example/asier/cebanc_burger/DatosCliente.java
+++ app/src/main/java/com/example/asier/cebanc_burger/DatosCliente.java
@@ -11,29 +11,25 @@ import android.widget.Toast;
 public class DatosCliente extends MainActivity {
     private Button siguiente;
     private Button salir;
-    private EditText nombre,apellidos,direccion,telefono;
-    private boolean verificar=false;
+    private EditText nombre, apellidos, direccion, telefono;
+    private boolean verificar = false;
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.datos_cliente);
 
         siguiente = (Button) findViewById(R.id.buttonSiguiente);
-        salir=(Button)findViewById(R.id.buttonSalir);
-        nombre=(EditText) findViewById(R.id.editTextNombre);
-        apellidos=(EditText)findViewById(R.id.editTextApellido);
-        direccion=(EditText)findViewById(R.id.editTextDireccion);
-        telefono=(EditText)findViewById(R.id.editTextTelefono);
+        salir = (Button) findViewById(R.id.buttonSalir);
+        nombre = (EditText) findViewById(R.id.editTextNombre);
+        apellidos = (EditText) findViewById(R.id.editTextApellido);
+        direccion = (EditText) findViewById(R.id.editTextDireccion);
+        telefono = (EditText) findViewById(R.id.editTextTelefono);
 
         siguiente.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-            //verificar=comprobacion();
-                if (verificar=true) {
-                    lanzarPedido();
-                }else {
-                    Toast.makeText(DatosCliente.this, "zepiii", Toast.LENGTH_SHORT).show();
-                }
+                verificar = comprobacion();
+                lanzarPedido(verificar);
             }
         });
 
@@ -45,46 +41,49 @@ public class DatosCliente extends MainActivity {
         });
     }
 
-    public void lanzarPedido(){
-
+    public void lanzarPedido(boolean v) {
+        if (v == true) {
             Intent i = new Intent(DatosCliente.this, Hamburguesas.class);
             i.putExtra("nombre", nombre.getText().toString());
             i.putExtra("apellidos", apellidos.getText().toString());
             i.putExtra("direccion", direccion.getText().toString());
             i.putExtra("telefono", telefono.getText().toString());
             startActivityForResult(i, 1234);
-
+        }
     }
 
-   /*public boolean comprobacion(){
-      boolean verificar=false;
-        if(nombre.getText().toString().equals("") && apellidos.getText().toString().equals("") && direccion.getText().toString().equals("") && telefono.getText().toString().equals("")) {
+    public boolean comprobacion() {
+        boolean verificar = false;
+        if (nombre.getText().toString().equals("") && apellidos.getText().toString().equals("") && direccion.getText().toString().equals("") && telefono.getText().toString().equals("")) {
             Toast.makeText(this, "¡ Deberías introducir los datos para continuar !", Toast.LENGTH_SHORT).show();
             verificar = false;
-        }else if(nombre.getText().toString().equals("") || apellidos.getText().toString().equals("") || telefono.getText().toString().equals("") ){
-            if(nombre.getText().toString().equals("")){
-                Toast.makeText(this, "¡ Deberías introducir los datos para continuar !", Toast.LENGTH_SHORT).show();
-                verificar=false;
-            }if(apellidos.getText().toString().equals("")){
-                Toast.makeText(this, "¡ Deberías introducir los datos para continuar !", Toast.LENGTH_SHORT).show();
-                verificar=false;
-            }if(direccion.getText().toString().equals("")){
-                Toast.makeText(this, "¡ Deberías introducir los datos para continuar !", Toast.LENGTH_SHORT).show();
-                verificar=false;
-            }if(telefono.getText().toString().equals("")){
-                Toast.makeText(this, "¡ Deberías introducir los datos para continuar !", Toast.LENGTH_SHORT).show();
-                verificar=false;
+        } else if (nombre.getText().toString().equals("") || apellidos.getText().toString().equals("") || telefono.getText().toString().equals("")) {
+            if (nombre.getText().toString().equals("")) {
+                nombre.setError("Introduce el nombre");
+                verificar = false;
             }
+            if (apellidos.getText().toString().equals("")) {
+                apellidos.setError("Introduce el apellido");
+                verificar = false;
+            }
+            if (direccion.getText().toString().equals("")) {
+                direccion.setError("Introduce la direccion");
+                verificar = false;
+            }
+            if (telefono.getText().toString().equals("")) {
+                telefono.setError(" Introduce el telefono");
+                verificar = false;
+            }
+        } else {
+            verificar = true;
         }
-        verificar=true;
         return verificar;
     }
-*/
-    public void salir(){
+
+    public void salir() {
 
         finish();
     }
-
 
 
 }
